f1bdac9bf5a683a05a6dd47f874be4b1956280c0
SHU-red <schuhmann.sebastian@gmail.com>
2022-07-19 16:19:02 +0200

    Fininshed Script with CANPico - Read Message - Only set relevant bit - Send Message

--- main.py
+++ main.py
@@ -7,15 +7,36 @@ import time
 # Define LED-Pin
 led = Pin(25, Pin.OUT)
 
+# LED ON
+led.value(True)
+
+# Filter IDs
+id_filter = {0: CANID(0x1A9).get_id_filter()}
+
 # Initialize CAN controller
-c = CAN(profile=BITRATE_125K_75,mode=NORMAL)
+# profile 2: 125 kBaud
+# Only use ID 1A9
+c = CAN(profile=CAN.BITRATE_125K_75,id_filters=id_filter)
 
 # Wait for 20s
 print("=== Wait for 20s")
 time.sleep(20)
 
+# Read frame
+frames = c.recv()
+payload = frames[0].get_data()
+
+# Set Bit 7 on Byte 6
+a = payload
+b = b'\x00\x00\x00\x00\x00\x00\xD0\x00'
+payload = (int.from_bytes(a, 'big') | int.from_bytes(b, 'big')).to_bytes(max(len(a), len(b)), 'big')
+
+# Notify
+print("Payload for sent message")
+print(payload)
+
 # Define CANFrame
-f = CANFrame(CANID(0x1A9), dlc=8, data=b'\x20\x7F\xFF\x00\xFF\x00\xD0\x00')
+f = CANFrame(CANID(0x1A9), data=payload)
 
 # Send Frame
 print("=== Sending CAN Frame")

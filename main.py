24f6c3dd59939346211f7de2c9b16bb7913567d0
SHU-red <schuhmann.sebastian@gmail.com>
2022-07-20 08:06:13 +0200

    Improved code for faster/better debugging and offline-testing Using if-statement to switch between vehicle and offline-testing

--- main.py
+++ main.py
@@ -14,17 +14,38 @@ led.value(True)
 id_filter = {0: CANID(0x1A9).get_id_filter()}
 
 # Initialize CAN controller
+print("=== Initializing CAN")
 # profile 2: 125 kBaud
 # Only use ID 1A9
 c = CAN(profile=CAN.BITRATE_125K_75,id_filters=id_filter)
 
-# Wait for 20s
-print("=== Wait for 20s")
-time.sleep(20)
+
 
 # Read frame
-frames = c.recv()
-payload = frames[0].get_data()
+
+
+if True: # True for vehicle, False for offline Testing
+    
+    # Wait for 30s
+    print("=== Wait for 30s")
+    time.sleep(30)
+    
+    # Read frame
+    frames = c.recv() # For Vehicle
+    payload = frames[0].get_data() # For Vehicle
+    
+else:
+    
+    # Wait for 2s --> Faster Testing
+    print("=== Wait for 2s (TESTING)")
+    time.sleep(2)
+    payload = b'\x20\x7F\xFF\x00\xFF\x00\x50\x00'
+
+# Output payload
+print("=== Read payload from CAN")
+print(payload)
+
+
 
 # Set Bit 7 on Byte 6
 a = payload
@@ -32,15 +53,15 @@ b = b'\x00\x00\x00\x00\x00\x00\x80\x00'
 payload = (int.from_bytes(a, 'big') | int.from_bytes(b, 'big')).to_bytes(max(len(a), len(b)), 'big')
 
 # Notify
-print("Payload for sent message")
+print("=== New payload for Sending")
 print(payload)
 
 # Define CANFrame
 f = CANFrame(CANID(0x1A9), data=payload)
 
 # Send Frame
-print("=== Sending CAN Frame")
 c.send_frame(f)
+print("=== Sent message")
 
 # Go to infinite Standby
 print("=== Standby [LED BLINK SLOW])")

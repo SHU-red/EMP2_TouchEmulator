0438caf71e5110da56beec2914e0622d2b80cf9c
SHU-red <schuhmann.sebastian@gmail.com>
2022-08-03 19:00:48 +0200

    Big Improvement regarding robustness (Sometimes deactivation did not work, due to the additional messages, sent from PiCAN have not been read from the vehicle (because this way more samples have been sent than usual)
    Read current StSt Status
    Send Toggle bit to turn off
    Repeat if StSt Status did not change to OFF

--- main.py
+++ main.py
@@ -10,55 +10,87 @@ led = Pin(25, Pin.OUT)
 # LED ON
 led.value(True)
 
+# In-work indicator to false
+inwork = True
+
+# Loop counter
+loop = 1
+
+# Waiting for vehicle to poperly boot
+print("=== Wait for 30s")
+time.sleep(30)
+
 # Filter IDs
-id_filter = {0: CANID(0x1A9).get_id_filter()}
+id_filter_state = {0: CANID(0x227).get_id_filter()}
+id_filter_toggle = {0: CANID(0x1A9).get_id_filter()}
 
-# Initialize CAN controller
-print("=== Initializing CAN")
-# profile 2: 125 kBaud
-# Only use ID 1A9
-c = CAN(profile=CAN.BITRATE_125K_75,id_filters=id_filter)
+# Repeat work until StSt is off
+while inwork:
 
-# Switch between vehicle and offline testing
-if True: # True for vehicle, False for offline testing
-    
-    # Wait for 30s
-    print("=== Wait for 30s")
-    time.sleep(30)
-    
-    # Read frame
-    frames = c.recv() # For Vehicle
-    payload = frames[0].get_data() # For Vehicle
-    
-else:
-    
-    # Wait for 2s --> Faster Testing
-    print("=== Wait for 2s (TESTING)")
+    # Initialize CAN controller for reading StSt state
+    print("=== Initializing CAN")
+    c = CAN(profile=CAN.BITRATE_125K_75,id_filters=id_filter_state)
     time.sleep(2)
-    payload = b'\x20\x7F\xFF\x00\xFF\x00\x50\x00'
 
-# Output payload
-print("=== Read payload from CAN")
-print(payload)
+    # Read payload
+    frames = c.recv()
+    payload_state = frames[0].get_data()
 
-# Set Bit 7 on Byte 6
-a = payload
-b = b'\x00\x00\x00\x00\x00\x00\x80\x00'
-payload = (int.from_bytes(a, 'big') | int.from_bytes(b, 'big')).to_bytes(max(len(a), len(b)), 'big')
+    # Output payload
+    print("=== StSt State frame 227")
+    print(payload_state)
 
-# Notify
-print("=== New payload for Sending")
-print(payload)
+    # Check for Bit2 in Byte3
+    a = payload_state
+    b = b'\x00\x00\x00\x04\x00\x00\x00\x00'
+    a = (int.from_bytes(a, 'big') & int.from_bytes(b, 'big')).to_bytes(max(len(a), len(b)), 'big')
+    n = b'\x00\x00\x00\x00\x00\x00\x00\x00'
 
-# Define CANFrame
-f = CANFrame(CANID(0x1A9), data=payload)
+    # If Bit 2 of Byte 3 is set = StSt OFF
+    if a != n:
 
-# Send Frame
-c.send_frame(f)
-print("=== Sent message")
+        print("=== StSt is now OFF")
+        inwork = False
 
-# Go to infinite Standby
-print("=== Standby [LED BLINK SLOW])")
+    else:
+
+        # Initialize CAN controller for reading StSt toggle
+        c = CAN(profile=CAN.BITRATE_125K_75,id_filters=id_filter_toggle)
+        time.sleep(2)
+
+        # Read payload
+        frames = c.recv()
+        payload = frames[0].get_data()
+
+        # Output payload
+        print("=== StSt State frame 227")
+        print(payload)
+
+        # Set Bit 7 on Byte 6
+        a = payload
+        b = b'\x00\x00\x00\x00\x00\x00\x80\x00'
+        payload = (int.from_bytes(a, 'big') | int.from_bytes(b, 'big')).to_bytes(max(len(a), len(b)), 'big')
+
+        # Notify
+        print("=== New payload for Sending")
+        print(payload)
+
+        # Define CANFrame
+        f = CANFrame(CANID(0x1A9), data=payload)
+
+        # Send Frame
+        c.send_frame(f)
+        print("=== Sent message")
+
+    print("Loop:")
+    print(loop)
+    print("==============================")
+    loop += 1
+
 while True:
+
+    # Go to infinite Standby
+    print("=== Standby [LED BLINK SLOW])")
+
     led.toggle()
-    time.sleep(2)
+    time.slaeep(2)
